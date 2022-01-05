902763e19ce4e3d3d38c68263bc38286a09cf5ca
artiskw <artiskerpew@gmail.com>
2022-01-05 04:52:20 +0200

    fix menu_nav

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//nav_scrolled
+/*Nav Scrolled*/
+
 const navbar = document.querySelector('.navbar_container'); //atlasam elementu 
 const sectionOne = document.querySelector('.home_content'); 
 
@@ -17,7 +18,7 @@ const sectionOneObserver = new IntersectionObserver
               navbar.classList.add('nav_scrolled');
              
           } else {
-            navbar.classList.remove('nav_scrolled')
+            navbar.classList.remove('nav_scrolled');
           }
         });
     }, 
@@ -25,9 +26,36 @@ sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
 
-// Mobile menu
+/* Nav Menu */
+
+let hamburger = document.querySelector('.hamburger_icon');
+let navMenu = document.querySelector('nav');
+
+
+hamburger.addEventListener("click", mobileMenu);
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+
+let navLink = document.querySelectorAll('a'); //kapec negaja uz nav_link?
+
+navLink.forEach(n => n.addEventListener("click", closeMenu)); //
+
+function closeMenu() {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }
+
+
+
+
+/* 
     document.querySelector('.nav_menu_icon').onclick = function (event) {
       document.querySelector('body').classList.toggle('active');
       document.querySelector('.home_content').classList.toggle('active')
       document.querySelector('.navbar_active nav').classList.toggle('active');
   }
+  */
