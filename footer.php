1d4450fa87135bc777ee705cea724cec80200d16
artiskw <81115271+artiskw@users.noreply.github.com>
2022-06-30 03:12:08 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="copy_right">
+        <p>© Copyright 2022 My Portofilio - All rights reserved.</p>
+    </div>
+</footer>
+<script src="<?php echo get_template_directory_uri();?>/script.js"></script>
